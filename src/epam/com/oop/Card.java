14afe1035e5eb46e7e124bc1e1f865360c1b3e6e
Tatsiana Marfel <tatsiana_marfel@epam.com>
2017-11-28 07:13:21 +0300

    Add assertions to Card class

--- src/epam/com/oop/Card.java
+++ src/epam/com/oop/Card.java
@@ -99,4 +99,27 @@ public class Card {
 		}
 	}
 
+	public static void main(String[] args) {
+
+		assert rankToString(ACE) == "Ace";
+		assert rankToString(DEUCE) == "Deuce";
+		assert rankToString(THREE) == "Three";
+		assert rankToString(FOUR) == "Four";
+		assert rankToString(FIVE) == "Five";
+		assert rankToString(SIX) == "Six";
+		assert rankToString(SEVEN) == "Seven";
+		assert rankToString(EIGHT) == "Eight";
+		assert rankToString(NINE) == "Nine";
+		assert rankToString(TEN) == "Ten";
+		assert rankToString(JACK) == "Jack";
+		assert rankToString(QUEEN) == "Queen";
+		assert rankToString(KING) == "King";
+
+		assert suitToString(DIAMONDS) == "Diamonds";
+		assert suitToString(CLUBS) == "Clubs";
+		assert suitToString(HEARTS) == "Hearts";
+		assert suitToString(SPADES) == "Spades";
+
+	}
+
 }
--- src/epam/com/oop/Runner.java
+++ src/epam/com/oop/Runner.java
@@ -1,5 +1,13 @@
 package epam.com.oop;
 
 public class Runner {
-
+	public static void main(String[] args) {
+		Deck deck = new Deck();
+		for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) {
+			for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+				Card card = deck.getCard(suit, rank);
+				System.out.format("%s of %s%n", card.rankToString(card.getRank()), card.suitToString(card.getSuit()));
+			}
+		}
+	}
 }

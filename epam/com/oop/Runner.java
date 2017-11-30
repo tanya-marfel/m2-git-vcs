5e6f4cfc6f0ec540382ce53273ba40db985091d1
Tatsiana Marfel <tatsiana_marfel@epam.com>
2017-11-30 21:20:42 +0300

    Revert "Update Runner and Deck3 classes"
    This reverts commit fc30a5ca9ed11b89cfb09b0028447a7aed4799c2

--- epam/com/oop/Runner.java
+++ epam/com/oop/Runner.java
@@ -2,8 +2,12 @@ package epam.com.oop;
 
 public class Runner {
 	public static void main(String[] args) {
-		Deck3 deck = new Deck3();
-		deck.getCard();
-
+		Deck deck = new Deck();
+		for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) {
+			for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+				Card card = deck.getCard(suit, rank);
+				System.out.format("%s of %s%n", card.rankToString(card.getRank()), card.suitToString(card.getSuit()));
+			}
+		}
 	}
 }
